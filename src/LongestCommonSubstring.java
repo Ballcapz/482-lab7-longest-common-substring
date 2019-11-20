0ef7810bcf23c0981be2bded46227d0891497603
Zach Johnson <zlg23johnson@gmail.com>
2019-11-19 18:30:04 -0600

    Got the faster lcs algo working, had to modify the memoization a bit from my initial thoughts to get the starting index of the lcs in each of the string.

--- src/LongestCommonSubstring.java
+++ src/LongestCommonSubstring.java
@@ -24,4 +24,42 @@ public class LongestCommonSubstring {
         return returnObject;
     }
 
+    public int[] fasterLCS(String s1, String s2) {
+        int length1 = s1.length();
+        int length2 = s2.length();
+        int i, j;
+
+        int[][] memo = new int[length1][length2];
+
+        int[] returnObject = new int[3];
+
+        for (i = 0; i < length1; i++) {
+            for (j = 0; j < length2; j++) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    if (i == 0 || j == 0) {
+                        memo[i][j] = 1;
+                    } else {
+                        memo[i][j] = memo[i - 1][j - 1] + 1;
+                        returnObject[1] = i;
+                        returnObject[2] = j;
+                    }
+
+                    if (returnObject[0] < memo[i][j]) {
+                        returnObject[0] = memo[i][j];
+
+                    }
+                }
+            }
+        }
+
+        if (returnObject[0] == 0) {
+            return returnObject;
+        }
+
+        returnObject[1] = returnObject[1] - returnObject[0] + 1;
+        returnObject[2] = returnObject[2] - returnObject[0] + 1;
+        return returnObject;
+
+    }
+
 }
--- src/Runner.java
+++ src/Runner.java
@@ -3,14 +3,20 @@ class Runner {
     public static void main(String[] args) {
         LongestCommonSubstring lcs = new LongestCommonSubstring();
 
-        String test1 = "Hello";
-        String test2 = "bell";
+        String test1 = "bbbbbbbb";
+        String test2 = "zzzzzz";
 
         int[] result = new int[3];
+        int[] result2 = new int[3];
 
         result = lcs.bruteForceLCS(test1, test2);
 
+        result2 = lcs.fasterLCS(test1, test2);
+
         System.out.printf("Length: %d\n I: %d\n J: %d\n", result[0], result[1], result[2]);
+        System.out.printf("Length: %d\n I: %d\n J: %d\n", result2[0], result2[1], result2[2]);
     }
+
+    // correctness testing
 
 }
