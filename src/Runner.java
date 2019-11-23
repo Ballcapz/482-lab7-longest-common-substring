bed4395750ee9a8dcd418e7727ba3683cb22fb1a
Zach Johnson <zlg23johnson@gmail.com>
2019-11-23 10:17:02 -0600

    Added stopwatch class and cleaned up main

--- src/Runner.java
+++ src/Runner.java
@@ -4,30 +4,11 @@ import java.util.Random;
 class Runner {
 
     public static void main(String[] args) {
-        // LongestCommonSubstring lcs = new LongestCommonSubstring();
 
-        // String test1 = "bbbbbbbb";
-        // String test2 = "zzzzzz";
-
-        // int[] result = new int[3];
-        // int[] result2 = new int[3];
-
-        // result = lcs.bruteForceLCS(test1, test2);
-
-        // result2 = lcs.fasterLCS(test1, test2);
-
-        // System.out.printf("Length: %d\n I: %d\n J: %d\n", result[0], result[1],
-        // result[2]);
-        // System.out.printf("Length: %d\n I: %d\n J: %d\n", result2[0], result2[1],
-        // result2[2]);
-
-        // System.out.println(generateRandomStringOfLength(10));
-
-        testLCSFunctions();
+        // testLCSFunctions();
 
     }
 
-    // correctness testing
     public static String generateRandomStringOfLength(int length) {
         int leftLimit = 97; // letter 'a'
         int rightLimit = 122; // letter 'z'
@@ -43,6 +24,7 @@ class Runner {
         return generatedString;
     }
 
+    // correctness testing
     public static void testLCSFunctions() {
 
         LongestCommonSubstring lcs = new LongestCommonSubstring();
